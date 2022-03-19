97fb0dc89142a5ceae3cd96f63a0020c67663cc6
Victor Molina <victormolinalvp@gmail.com>
2022-03-19 17:58:12 -0300

    Primer commit del punto numero 1

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola Mundo!");
+    int variable1 = 5, *pvar1;
+    printf("Ingrese un numero: ");
+    scanf("%d",&variable1);
+
+    pvar1=&variable1;
+
+    printf("El contenido del puntero es: %d\n",*pvar1);
+    printf("La direccion de memoria almacenada por el puntero es: %d\n", pvar1);
+    printf("Direccion de memoria de la variable: %d\n",&variable1);
+    printf("Direccion de memoria del puntero: %d\n", pvar1);
+    printf("Tamanio de memoria utilizado por la variable: %d\n", sizeof(pvar1));
 
     return 0;
 }
